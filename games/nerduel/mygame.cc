76729a9fe9eb2a12a9d28aa76cb369a7b831359c
jeffxzy <55473481+jeffxzy@users.noreply.github.com>
2022-09-25 11:49:20 +0800

    Revert "nerduel: add turn limit to avoid infinite game"
    This reverts commit f7c8858616114970260ef7130ecad4d659bb5ffc.

--- games/nerduel/mygame.cc
+++ games/nerduel/mygame.cc
@@ -138,7 +138,6 @@ class MainStage : public MainGameStage<RoundStage> {
 
   bool ended_;
   MyTable table_;
-  int turn_;
   std::array<std::string, 2> target_;
   std::array<int64_t, 2> score_;
 };
@@ -237,8 +236,7 @@ class GuessingStage : public SubGameStage<> {
 
 class RoundStage : public SubGameStage<SettingStage, GuessingStage> {
  public:
-  RoundStage(MainStage& main_stage)
-      : GameStage(main_stage, "第" + std::to_string(main_stage.turn_) + "回合") {}
+  RoundStage(MainStage& main_stage) : GameStage(main_stage, "你的回合") {}
 
   virtual VariantSubStage OnStageBegin() override {
     return std::make_unique<SettingStage>(main_stage());
@@ -254,11 +252,6 @@ class RoundStage : public SubGameStage<SettingStage, GuessingStage> {
 
   virtual VariantSubStage NextSubStage(GuessingStage& sub_stage,
                                        const CheckoutReason reason) override {
-    if (++main_stage().turn_ > 50) {
-      Boardcast() << "回合数超过上限，游戏结束。";
-      main_stage().ended_ = true;
-      return {};
-    }
     if (main_stage().ended_) {
       return {};
     }
@@ -270,7 +263,7 @@ class RoundStage : public SubGameStage<SettingStage, GuessingStage> {
 };
 
 MainStage::MainStage(const GameOption& option, MatchBase& match)
-    : GameStage(option, match), ended_(false), table_(option), turn_(1) {}
+    : GameStage(option, match), ended_(false), table_(option) {}
 
 MainStage::VariantSubStage MainStage::OnStageBegin() { return std::make_unique<RoundStage>(*this); }
 
